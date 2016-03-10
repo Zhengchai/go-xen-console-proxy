224520641d12bb01f57c967488c1665cd69f67a0
Syed <syed1.mushtaq@gmail.com>
2016-03-10 11:08:28 -0500

    remove unused utils.go

--- utils.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "regexp"
-
-func IsUUID(text string) bool {
-	r := regexp.MustCompile("^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[8|9|aA|bB][a-f0-9]{3}-[a-f0-9]{12}$")
-	return r.MatchString(text)
-}
